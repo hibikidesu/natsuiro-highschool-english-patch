b086e60fd8012ffc82cf0219f1f160777fe729eb
hibiki <20015683+hibikidesu@users.noreply.github.com>
2020-11-30 17:30:32 +0800

    create helptext bin func

--- test.py
+++
@@ -1,4 +0,0 @@
-import tools
-
-
-print(tools.extract_helptext_bin("/x/Games/BLJS10273_INSTALLDATA/USRDIR/DSC/Install/npg10/help/HelpText.bin"))
--- tools/helptext.py
+++ tools/helptext.py
@@ -2,7 +2,7 @@ from struct import unpack, pack
 from functools import partial
 from ctypes import create_string_buffer
 
-__all__ = ["extract_helptext_bin"]
+__all__ = ["extract_helptext_bin", "create_helptext_bin"]
 
 
 def extract_helptext_bin(file_path: str) -> list:
@@ -24,3 +24,22 @@ def extract_helptext_bin(file_path: str) -> list:
             text = create_string_buffer(unpacked[1]).value.decode("cp932")
             data.append([unpacked[0], text])
     return data
+
+
+def create_helptext_bin(file_path: str, data: list):
+    """
+    Creates a helptext.bin with a given filepath
+
+    Args:
+        file_path (str): filepath to create the helptext.bin file
+        data (List[int, str]): data to add to file, the list must first contain an int of a 4 byte number and string of text under 144 chars 
+
+    Raises:
+        ValueError: if str char count not under 144 characters
+    """
+    with open(file_path, "wb") as f:
+        for item in data:
+            print(item)
+            if len(item[1]) >= 0x90:
+                raise ValueError(f"'{item[1]}' exceeds character count ({len(item[1])}/143")
+            f.write(pack(">I90s", item[0], item[1].encode("cp932")))
