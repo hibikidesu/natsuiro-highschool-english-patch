0794cc7504abd7f563fb7b316011fcab3deaa2d8
hibiki <20015683+hibikidesu@users.noreply.github.com>
2020-12-03 06:45:49 +0800

    move translation to npg10

--- build_translation.py
+++ build_translation.py
@@ -4,33 +4,33 @@ import tools
 
 
 def build_text():
-    os.makedirs(os.path.join("translated", "text"), exist_ok=True)
+    os.makedirs(os.path.join("translated", "npg10", "text"), exist_ok=True)
     with open("translated_text.json", "r", encoding="cp932") as f:
         data = json.load(f)
     for file in data:
-        tools.create_text_bin(os.path.join("translated", "text", file), data[file])
+        tools.create_text_bin(os.path.join("translated", "npg10", "text", file), data[file])
         print(f"Created text/{file}")
 
 
 def build_helptext():
-    os.makedirs(os.path.join("translated", "help"), exist_ok=True)
+    os.makedirs(os.path.join("translated", "npg10", "help"), exist_ok=True)
     with open("translated_helptext.json", "r", encoding="cp932") as f:
         data = json.load(f)
     out = []
     for x in data:
         out.append([int(x), data[x]])
-    tools.create_helptext_bin(os.path.join("translated", "help", "HelpText.bin"), out)
+    tools.create_helptext_bin(os.path.join("translated", "npg10", "help", "HelpText.bin"), out)
     print("Created help/HelpText.bin")
 
 
 def build_tutorial():
-    os.makedirs(os.path.join("translated", "tutorial"), exist_ok=True)
+    os.makedirs(os.path.join("translated", "npg10", "tutorial"), exist_ok=True)
     with open("translated_tutorial.json", "r", encoding="cp932") as f:
         data = json.load(f)
     out = []
     for x in data:
         out.append(data[x])
-    tools.create_tutorial_bin(os.path.join("translated", "tutorial", "Tutorial.bin"), out)
+    tools.create_tutorial_bin(os.path.join("translated", "npg10", "tutorial", "Tutorial.bin"), out)
     print("Created tutorial/Tutorial.bin")
 
 
